82b0fbb25892e405411580e437ea08281c1ada5f
hasukawashizuo <657783746@qq.com>
2021-04-30 00:47:29 +0800

    327

---
+++ 327.c/327.c/test.c
@@ -0,0 +1,32 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+//int main()
+//{
+	/*int num1 = 0;
+	int num2 = 0;
+	int sum = 0;
+	scanf("%d%d", &num1, &num2);
+	sum = num1 + num2;
+	printf("sum = %d\n", sum);
+	return 0;*/
+	/*int num = 4;
+	printf("%d\n", num);
+	num = 8;
+	printf("%d\n", num);
+	return 0;*/
+	/*const int n = 10;
+	int arr[n] = { 0 };
+	return 0;*/
+//int main()
+//{
+//	/*char arr1[] = "abc";
+//	char arr2[] = { 'a', 'b', 'c','\0'};
+//	printf("%s\n", arr1);
+//	printf("%s\n", arr2);
+//	return 0;*/
+//	char arr1[] = "abc";
+//	char arr2[] = { 'a', 'b', 'c' };
+//	printf("%d\n", strlen(arr1));
+//	printf("%d\n", strlen(arr2));
+//	return 0;
+//}
